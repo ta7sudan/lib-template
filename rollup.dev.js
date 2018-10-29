f7a6586638cd953fea1f257a5ffa6c4f85be1de7
ta7sudan <ta7sudan@gmail.com>
2018-10-29 18:02:48 +0800

    update

--- rollup.dev.js
+++ rollup.dev.js
@@ -3,27 +3,25 @@ import replace from 'rollup-plugin-replace';
 import { relative } from 'path';
 import { name } from './package.json';
 
-export default [
+export default {
+	input: 'src/index.js',
+	plugins: [
+		replace({
+			DEBUG: JSON.stringify(true)
+		}),
+		babel({
+			exclude: 'node_modules/**'
+		})
+	],
+	treeshake: {
+		propertyReadSideEffects: false
+	},
+	output:
 	{
-		input: 'src/index.js',
-		plugins: [
-			replace({
-				DEBUG: JSON.stringify(true)
-			}),
-			babel({
-				exclude: 'node_modules/**'
-			})
-		],
-		treeshake: {
-			propertyReadSideEffects: false
-		},
-		output:
-		{
-			name,
-			file: 'example/scripts/{{bundleName}}.umd.js',
-			format: 'umd',
-			sourcemap: true,
-			sourcemapPathTransform: path => ~path.indexOf('index') ? '{{bundleName}}.js' : relative('src', path)
-		}
+		name,
+		file: 'example/scripts/{{bundleName}}.umd.js',
+		format: 'umd',
+		sourcemap: true,
+		sourcemapPathTransform: path => ~path.indexOf('index') ? '{{bundleName}}.js' : relative('src', path)
 	}
-];
+};
