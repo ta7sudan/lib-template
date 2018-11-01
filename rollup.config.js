ea9190b3650f2244f7565661854bc2304f056b7e
ta7sudan <ta7sudan@gmail.com>
2018-11-01 16:43:36 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,13 @@ import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 {{if compressTool === 'uglify'}}import { uglify } from 'rollup-plugin-uglify';{{/if}}{{if compressTool === 'minify'}}import minify from 'rollup-plugin-babel-minify';{{/if}}
 import { relative } from 'path';
-import { browser, module, name, version, license, author, homepage } from './package.json';
+import { browser, module, main, name, version, license, author, homepage } from './package.json';
 
 /**
  * 如果用babel-minify压缩的话, banner字符串的开头和结尾谜之不能换行
  * 不过有一点好的是, 用rollup的banner字段和babel-minify的banner字段都可以
  * uglify的话则需要自己处理下注释
  */
-/* eslint-disable-next-line */
 const banner = `/**
  * @Version ${version}
  * @Author: ${author}
@@ -71,14 +70,22 @@ export default [
 		treeshake: {
 			propertyReadSideEffects: false
 		},
-		output: {
-			name,
-			// banner,
-			file: 'dist/{{bundleName}}.min.js',
-			format: 'umd',
-			sourcemap: true,
-			// sourcemap生成之后在devtools本来看到的文件是src/index.js, 这个选项可以变成{{bundleName}}.js
-			sourcemapPathTransform: path => ~path.indexOf('index') ? '{{bundleName}}.js' : relative('src', path)
-		}
+		output: [
+			{
+				name,
+				banner,
+				file: 'dist/{{bundleName}}.min.js',
+				format: 'umd',
+				sourcemap: true,
+				// sourcemap生成之后在devtools本来看到的文件是src/index.js, 这个选项可以变成{{bundleName}}.js
+				sourcemapPathTransform: path => ~path.indexOf('index') ? '{{bundleName}}.js' : relative('src', path)
+			},
+			{
+				banner,
+				file: main,
+				format: 'cjs',
+				sourcemap: true
+			}
+		]
 	}
 ];
